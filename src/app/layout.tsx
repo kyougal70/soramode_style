039e9e40adb9b53866523c18c25af7bc7633d81f
Yougal kumar <kyougal70@gmail.com>
2025-11-21 09:45:23 +0530

    add lucky and finger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=PT+Sans:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap"
           rel="stylesheet"
         />
+          <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=b459f5a4"></script>
           {/* Google Tag Manager Script */}
           <script
               src="https://www.googletagmanager.com/gtag/js?id=AW-17642653097"
@@ -63,8 +64,7 @@ export default function RootLayout({
       >
       <FpjsProvider
           loadOptions={{
-            apiKey: 'IOgMXJUD3mhMjcaTMJVn',
-            region: 'ap'
+            apiKey: 'fXFDKwB88JiT364PCsPN',
           }}
       >
         <PreferenceProvider>
